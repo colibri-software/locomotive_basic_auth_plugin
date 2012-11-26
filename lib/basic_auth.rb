1ecde934550e59d7ebfe07e5071ec581d835e495
Alex Sanford <alex.sanford1@gmail.com>
2012-11-26 13:40:21 -0400

    Added auth functionality

--- lib/basic_auth.rb
+++ lib/basic_auth.rb
@@ -2,9 +2,38 @@
 require 'rubygems'
 require 'bundler/setup'
 
+# This is a quick-and-dirty basic authentication plugin. The login string and
+# password are stored in plaintext in the config hash, so it is not secure. The
+# configuration also takes a regular expression which specifies the page
+# fullpaths which require basic authentication
 module BasicAuth
   class BasicAuth
+
     include Locomotive::Plugin
+
+    before_filter :authenticate_if_needed
+
+    def config_template_file
+      File.join(File.dirname(__FILE__), 'basic_auth', 'config.haml')
+    end
+
+    def authenticate_if_needed
+      authenticate if need_to_authenticate?
+    end
+
+    protected
+
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == config['login'] && password == config['password']
+      end
+    end
+
+    def need_to_authenticate?
+      controller.params['path'] =~ Regexp.new(
+        config['fullpath_regex'].gsub(/^\/+|\/+$/, ''))
+    end
+
   end
 
   LocomotivePlugins.register_plugin(BasicAuth)
